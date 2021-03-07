468a14e8b6487652acf3cfa7a5836933074a1b21
f-giftagram <florence@giftagram.com>
2021-03-07 02:07:24 -0700

    more for loop chanllenges

--- loops/for_loop.js
+++ loops/for_loop.js
@@ -1,7 +1,38 @@
-// write a javascript function to calculate the sum of a given set of numbers using a for loop
+// CHALLENGE 1
+//using a for loop write a javascript function to calculate 
+//the sum of a given set of numbers using a for loop
 
 function sumOfIntegers(numbers) {
     //write your code here
 }
 
 console.log(sumOfIntegers([1,2,3,4,5]))
+
+// CHALLENGE 2
+// =====================================================
+// using a for loop write a javascript function to 
+// compute the factorial of a give number 
+
+function factorial(number) {
+    //code here
+}
+
+
+
+//CHALLENGE 3
+// using a for loop, output the cost of licing in each city:
+// e.g Cost of living in LA is USD 77 
+
+function costOfliving() {
+    //code here
+}
+
+let cities = {
+    "LA": 77,
+    "Miami": 74,
+    "Toronto": 78,
+    "Boston": 83,
+    "DC": 87
+}
+
+console.log(costOfliving(cities))
