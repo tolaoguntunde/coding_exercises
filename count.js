97d6722ce3f4a9037350559e968babe1c9222be3
f-giftagram <florence@giftagram.com>
2021-03-07 02:21:27 -0700

    more challenges

---
+++ count.js
@@ -0,0 +1,21 @@
+// CHALLENGE 1:
+// write a javascript function to output the length of a given string
+// e.g Your name is 12 characters long
+
+function count() {
+    //code here
+}
+
+console.log(count('Example'))
+
+
+//CHALLENGE 2
+// write a javascript function to return the first character in a given string
+
+
+//CHALLENGE 3
+// write a javascript function to return the last character in a given string
+
+
+//CHALLENGE 4
+// write a javascript function to return the nth character in a given string
