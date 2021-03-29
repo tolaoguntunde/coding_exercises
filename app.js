62bf28879ca765fb347f85f5a0456798b2b0c746
fokosun <okosunuzflorence@gmail.com>
2021-03-28 21:40:25 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let firstname = "florence"
+let lastname = "okosun"
+
+let lengthOfFirstName = firstname.length
+let lengthOfLastName = lastname.length
+
+console.log('Length of my first name is', lengthOfFirstName)
+console.log('Length of my last name is', lengthOfFirstName)
+
+//template literals
+//string interpolation
+// let greeting = `Hello ${firstname} ${lastname} welcome to my TedTalk.`;
+
+// console.log(greeting)
---
+++ fizzbuzz.js
@@ -0,0 +1,4 @@
+// write a program that 
+// outputs Fizz if a given number is divisible by 3
+// outputs Buzz if the number is dividible by 5
+// outputs FizzBuzz if the number is divisible by 3 and 5
