2a4d90c4f416ee7b7108add80146ce95a4c41f57
fokosun <okosunuzflorence@gmail.com>
2021-03-28 21:39:21 -0600

    Delete linear_search.js

--- linear_search.js
+++
@@ -1,7 +0,0 @@
-//write a js function that returns yes if a given number is found in an array
-// and false if not found
-// e.g find 2 in [0,1,2,4] should return true
-
-function linearSearch(numbers) {
-    // code here
-}
