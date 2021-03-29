5d2efb518418a1f7471eca6a8fb64d1f3801cdb2
fokosun <okosunuzflorence@gmail.com>
2021-03-28 21:38:56 -0600

    Delete circumference.js

--- circumference.js
+++
@@ -1,9 +0,0 @@
-// write a javascript function to calculate the circumference of a circle
-// passing the radius as an argument
-// let the radius be a default of 2 if the radous is not given
-
-function circumference(radius) {
-    // code here
-}
-
-console.log(circumference(7));
