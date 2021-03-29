abccabb158d4d3dc99efefa5dbdd7d97c7485aba
fokosun <okosunuzflorence@gmail.com>
2021-03-28 21:42:05 -0600

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,10 @@
+// check that a number is a multiple of 3
+
+let a = 4;
+let b = 2;
+
+let remainder = a % b;
+
+if (remainder === 0) {
+    console.log(a + ' is divisible by ' + b)
+}
---
+++ strings.js
@@ -0,0 +1,12 @@
+let firstname = "florence"
+
+// let firstCharPos = firstname[0]
+// firstCharPos = firstCharPos.toUpperCase()
+
+// let secondPos = firstname[1]
+
+// console.log(firstCharPos)
+// console.log(secondPos)
+
+let pos = firstname[8]
+console.log(pos)
