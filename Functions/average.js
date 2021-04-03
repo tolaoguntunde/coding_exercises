116df0c289698398564623ec90050928a1fdaf74
Tola Oguntunde <tolaoguntunde@gmail.com>
2021-04-03 13:14:03 -0500

    Created Average Calculator

--- Functions/average.js
+++ Functions/average.js
@@ -1,2 +1,15 @@
 //write a js function to calculate the average 
 //of a list of numbers
+
+let total =0;
+function calcAverage(number){
+for(i=0; i<number.length; i++)
+{
+    total=total+number[i]
+}
+
+return total/number.length;
+
+}
+
+console.log(calcAverage([20,20,10]))
