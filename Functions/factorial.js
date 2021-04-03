39cddba801a27ab6f98fa9d12a89b7eefc5dc57e
Tola Oguntunde <tolaoguntunde@gmail.com>
2021-04-03 14:08:57 -0500

    Factorial Calculator

--- Functions/factorial.js
+++ Functions/factorial.js
@@ -1,1 +1,13 @@
 //write a js function to compute the factorial of a given number N
+
+function calcFactorial(num){
+    multi =1
+
+for (i=1; i<num+1; i++){
+
+    multi *= i;
+}
+
+return multi;
+}
+console.log(calcFactorial(4))
